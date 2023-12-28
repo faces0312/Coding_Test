a5139a9936eca0438be524e9c582c6b5b5cbe973
HyunSeok Suh <suhya990312@gmail.com>
2023-12-28 14:57:10 +0900

    commit4

--- Practice.cs
+++ Practice.cs
@@ -1,1 +1,17 @@
 //Practice
+using System;
+
+namespace Test_Prepare_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split();
+            
+            int sum = int.Parse(str[0]) + int.Parse(str[1]);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
